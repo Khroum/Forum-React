fd92f16d286b9194bed8a35f6283ae3206902f91
Khroum <fladro224@gmail.com>
2020-05-05 16:37:57 +0200

    topic page update

--- pages/TopicsPage.js
+++ pages/TopicsPage.js
@@ -1,16 +1,25 @@
 import React, {Component} from 'react';
-import {View, ScrollView, StyleSheet, FlatList, RefreshControl, Text} from 'react-native';
-import {Loader} from "../component/Loader";
-import {API_URL, AUTH_HEADER} from "../utils/constants";
-import {TopicOverview} from "../component/TopicOverview";
-import {formatToDateTime} from "../utils/dateFormatter";
+import {
+  View,
+  ScrollView,
+  StyleSheet,
+  FlatList,
+  RefreshControl,
+  Text,
+} from 'react-native';
+import {Loader} from '../component/Loader';
+import {API_URL, POSTS_PAGE, TOPICS_PAGE} from '../utils/constants';
+import {TopicOverview} from '../component/TopicOverview';
+import {formatToDateTime} from '../utils/dateFormatter';
+import {goToScreenWithHeader, goToScreenWithProps} from '../utils/navbarHelper';
 
 export default class TopicsPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isFetching: false,
-      topics: []
+      isFetching: true,
+      topics: [],
+      header: '',
     };
   }
 
@@ -19,57 +28,70 @@ export default class TopicsPage extends Component {
   }
 
   onRefresh() {
-    this.setState({ isFetching: true}, () => {
-      this.getTopics()
+    this.setState({isFetching: true}, () => {
+      this.getTopics();
     });
   }
 
   getTopics = () => {
     const {header} = this.props;
-    console.log('topics header: ', header);
     fetch(`${API_URL}/topic/`, {
       headers: {
-        'Authorization': `${header}`
-      }
+        Authorization: `${header}`,
+      },
     })
-        .then(response => response.json())
-        .then(json => {
-          this.setState({
-            isFetching: false,
-            topics: json
-          });
-          console.log(this.state.topics);
-        })
-        .catch(error => {
-          console.error(error);
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          isFetching: false,
+          topics: json,
+          header: header,
         });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  goToPosts = (topicId) => {
+    goToScreenWithProps(
+      this.props.componentId,
+      POSTS_PAGE,
+      this.state.header,
+      topicId,
+    );
   };
 
   render() {
     if (this.state.isFetching === true) {
-      return (
-          <Loader/>
-      );
+      return <Loader />;
     } else {
       return (
-          <View>
-            <ScrollView refreshControl={
-              <RefreshControl refreshing={this.state.isFetching} onRefresh={() => this.onRefresh()}/>
-            }>
-              <FlatList data={this.state.topics}
-                        renderItem={({item}) =>
-                            <TopicOverview name={item.name}
-                                           description={item.description}
-                                           date={formatToDateTime(item.createdOn)} />
-                        }
-                        keyExtractor={(item, index) => index.toString()}
+        <View>
+          <ScrollView
+            refreshControl={
+              <RefreshControl
+                refreshing={this.state.isFetching}
+                onRefresh={() => this.onRefresh()}
               />
-            </ScrollView>
-          </View>
+            }>
+            <FlatList
+              data={this.state.topics}
+              renderItem={({item}) => (
+                <TopicOverview
+                  click={() => this.goToPosts(item.topicId)}
+                  name={item.name}
+                  description={item.description}
+                  date={formatToDateTime(item.createdOn)}
+                />
+              )}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          </ScrollView>
+        </View>
       );
     }
   }
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
